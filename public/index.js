5ea35d3ff2169198f6a80e9dfcd4a21628bed418
Kyle Romero <kromero@Kyles-MacBook-Pro.local>
2018-11-30 00:20:12 -0800

    added rotating home page

--- public/index.js
+++ public/index.js
@@ -1,36 +1,93 @@
 var user_logged = false
 
-var database = firebase.database();
+var mapping = ["one", "two", "three", "four"];
+var current_page = 0;
 
 var maxcount = 1;
 
 $(document).ready(function() {
   setUserLogged();
 
-  var maxcountRef = firebase.database().ref('imgs/max_count');
-  maxcountRef.once('value').then(function(snapshot) {
+  firebase.database().ref('imgs/max_count').once('value').then(function(snapshot) {
     if (snapshot.exists()) {
       maxcount = snapshot.val().max_count;
 
       var imgcount = 0;
-  for(var i = maxcount-1; i >= 0 && imgcount < 13; i--){
-    var databaseRef = firebase.database().ref('imgs/'+i);
-    databaseRef.once('value').then(function(snapshot) {
-      if (snapshot.exists()) {
-        var url = snapshot.val().url;
-        var caption = snapshot.val().caption;
-        var image1 = '<img src=' + url +' style="width:100%" onclick="onClick(this)" alt="'+caption+'">';
-        var imgnum = 'image' + imgcount;
-        document.getElementById(imgnum).innerHTML = image1;
-        imgcount++;
+      for(var i = maxcount-1; i >= 0 && imgcount < 13; i--){
+        var databaseRef = firebase.database().ref('imgs/'+i);
+        databaseRef.once('value').then(function(snapshot) {
+          if (snapshot.exists()) {
+            var url = snapshot.val().url;
+            var caption = snapshot.val().caption;
+            var image1 = '<img src=' + url +' style="width:100%" onclick="onClick(this)" alt="'+caption+'">';
+            var imgnum = 'image' + imgcount;
+            document.getElementById(imgnum).innerHTML = image1;
+            imgcount++;
+          }
+        });
       }
-    });
-  }
     }
   });
 });
 
+function one() {
+  $('#' + mapping[current_page]).removeClass("w3-black");
+  $('#' + mapping[current_page]).addClass("w3-hover-black");
+  current_page = 0;
+  $('#' + mapping[current_page]).removeClass("w3-hover-black");
+  $('#' + mapping[current_page]).addClass("w3-black");
+  reload();
+}
 
+function two() {
+  $('#' + mapping[current_page]).removeClass("w3-black");
+  $('#' + mapping[current_page]).addClass("w3-hover-black");
+  current_page = 1;
+  $('#' + mapping[current_page]).removeClass("w3-hover-black");
+  $('#' + mapping[current_page]).addClass("w3-black");
+  reload();
+}
+
+function three() {
+  $('#' + mapping[current_page]).removeClass("w3-black");
+  $('#' + mapping[current_page]).addClass("w3-hover-black");
+  current_page = 2;
+  $('#' + mapping[current_page]).removeClass("w3-hover-black");
+  $('#' + mapping[current_page]).addClass("w3-black");
+  reload();
+}
+
+function four() {
+  $('#' + mapping[current_page]).removeClass("w3-black");
+  $('#' + mapping[current_page]).addClass("w3-hover-black");
+  current_page = 3;
+  $('#' + mapping[current_page]).removeClass("w3-hover-black");
+  $('#' + mapping[current_page]).addClass("w3-black");
+  reload();
+}
+
+function reload () {
+  firebase.database().ref('imgs/max_count').once('value').then(function(snapshot) {
+    if (snapshot.exists()) {
+      maxcount = snapshot.val().max_count;
+
+      var imgcount = 0;
+      for(var i = maxcount-(1 + current_page * 12); i >= 0 && imgcount < 13; i--){
+        var databaseRef = firebase.database().ref('imgs/'+i);
+        databaseRef.once('value').then(function(snapshot) {
+          if (snapshot.exists()) {
+            var url = snapshot.val().url;
+            var caption = snapshot.val().caption;
+            var image1 = '<img src=' + url +' style="width:100%" onclick="onClick(this)" alt="'+caption+'">';
+            var imgnum = 'image' + imgcount;
+            document.getElementById(imgnum).innerHTML = image1;
+            imgcount++;
+          }
+        });
+      }
+    }
+  });
+}
 
 function setUserButtonHref() {
 	if (user_logged) {
