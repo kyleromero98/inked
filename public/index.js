fda83354f429c6d9c7ff9cb2f6c250db582acc23
Jair Hinojosa <JairHD011@gmail.com>
2018-11-30 00:15:26 -0800

    We could actually sign out now

--- public/index.js
+++ public/index.js
@@ -2,10 +2,22 @@ var user_logged = false
 
 $(document).ready(function() {
     setUserLogged();
+    setTopBarLinks();
 });
 
+function setTopBarLinks() {
+    setUserButtonHref();
+    if (user_logged) {
+        makeSignOutVisible();
+    }
+}
+
+function makeSignOutVisible() {
+    document.getElementById("sign_out_span").style.display = "inline-block";
+}
+
 function setUserButtonHref() {
-	if (user_logged) {
+    if (user_logged) {
     	document.getElementById("profile_button").href="profile.html";
 	}
 	else {
@@ -14,11 +26,20 @@ function setUserButtonHref() {
 }
 
 function setUserLogged() {
-	firebase.auth().onAuthStateChanged(function(user) {
-  		if (user) {
-  			user_logged = true;
-  		} else {
-    		user_logged = false;
-  		}
-	});
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        user_logged = true;
+        makeSignOutVisible();
+        setUserButtonHref();
+    }
+    else {
+        user_logged = false;
+    }
+    });
+}
+
+function signOut() {
+    firebase.auth().signOut().then(function() {
+        window.location.replace("index.html");
+    });
 }
